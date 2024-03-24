4d5def919a7d4a4e4114b32f85aee786298e07b7
SookX <vagrivas08@gmail.com>
2024-03-24 11:35:05 +0200

    user model

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'djoser',
+    'user_authenticate'
 ]
 
 MIDDLEWARE = [
--- backend/user_authenticate/models.py
+++ backend/user_authenticate/models.py
@@ -1,4 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-class User(models.Model):
-    email = models.
+def server_icon_upload_path(instance, filename):
+    return f"user/{instance.id}/profile_picutre/{filename}"
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, username, password = None):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        email = self.normalize_email(email) 
+        user = self.model(email = email, username = username)
+        user.set_password(password) 
+        user.save()
+        return user
+    
+    def create_superuser(self, email, username, password=None):
+
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length = 255, unique = True)
+    username = models.CharField(max_length = 255)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
+    profile_picture = models.ImageField(upload_to = server_icon_upload_path, default='default.png')
+
+    objects = UserAccountManager()
+    USERNAME_FIELD = 'email' 
+    REQUIRED_FIELDS = ['username']
+
+    def get_full_name(self):
+        return self.username
+
+    def get_short_name(self):
+        return self.username
+    
+    def __str__(self):
+        return self.email
