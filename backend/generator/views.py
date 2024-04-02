7da7e63238a303a7d25dc3904c9e43e43efc894f
Vasil Vasilev <89039078+SookX@users.noreply.github.com>
2024-04-02 23:28:46 +0300

    saved_image

--- backend/generator/views.py
+++ backend/generator/views.py
@@ -4,32 +4,37 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Image
 from django.contrib.auth import get_user_model
+import requests
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 
-from .sd import load_model, save_images, make_a_prediction_and_save
-TF_ENABLE_ONEDNN_OPTS=0
-sd_model = load_model()
-print("Model Loaded")
 
 UserAccount = get_user_model()
 
-
-def image_upload_path(owner, filename):
-    return f"user/{owner.id}/generated_image/{filename}"
 
 @api_view(['POST'])
 def genImage(request, *args, **kwargs):
     if request.method == "POST":
         body = request.body
         data = json.loads(body)
-        user_id = data['user_id']
         prompt = data['prompt']
         model = int(data['model'])
         print(prompt, model)
         if model == 0:
-            fig = make_a_prediction_and_save(prompt=prompt, model=sd_model)
-            filename = prompt
-            user = UserAccount.objects.filter(id = user_id).first()
-            image = Image(owner = user, prompt = prompt, image = fig)
-            image.save()
+            url = "https://197f-34-125-207-170.ngrok-free.app/"
+
+            data = {'prompt': prompt}
+
+            # Send the POST request
+            response = requests.post(url, json=data)
+
+            response_data = response.json()
+            response_data = np.array(response_data['data'], dtype=np.uint8)
+            
+            print("Image started saving")
+            plt.imshow(response_data)
+            plt.imsave("saved_image.png", response_data)
+
         print(prompt, model)
         return Response("Image generated successfully", status=200)
