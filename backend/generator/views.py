3d059fb415fee906a84ded3a549ed12cff176147
Vasil Vasilev <89039078+SookX@users.noreply.github.com>
2024-04-20 16:56:12 +0300

    cm

--- backend/generator/views.py
+++ backend/generator/views.py
@@ -13,7 +13,7 @@ import PIL
 from io import BytesIO
 
 UserAccount = get_user_model()
-NGROK_URL = "https://5d69-35-230-107-40.ngrok-free.app/"
+NGROK_URL = "https://38e1-34-82-47-43.ngrok-free.app"
 
 
 @api_view(['POST'])
@@ -24,21 +24,23 @@ def genImage(request, *args, **kwargs):
         id = int(data['id'])
         prompt = data['prompt']
         model = int(data['model'])
+
+        print(prompt, model)
         account = UserAccount.objects.filter(id = id).first()
         print(prompt, model)
-        if model == 0 or model == 1:
-            url = NGROK_URL
-            data = {'prompt': prompt}
-            response = requests.post(url, json=data)
-            response_data = response.json()
-            response_data = np.array(response_data['data'], dtype=np.uint8)
-            pil_image = PIL.Image.fromarray(response_data)
-            image_io = BytesIO()
-            pil_image.save(image_io, format='PNG')
-            image_io.seek(0)
-            img = Image(owner=account, promt=prompt)
-            img.image.save(f"{prompt}.png", image_io)
-            return Response("Added sucessfully")
+        
+        url = NGROK_URL
+        data = {'prompt': prompt, 'model': model}
+        response = requests.post(url, json=data)
+        response_data = response.json()
+        response_data = np.array(response_data['data'], dtype=np.uint8)
+        pil_image = PIL.Image.fromarray(response_data)
+        image_io = BytesIO()
+        pil_image.save(image_io, format='PNG')
+        image_io.seek(0)
+        img = Image(owner=account, promt=prompt)
+        img.image.save(f"{prompt}.png", image_io)
+        return Response("Added sucessfully")
 
         print(prompt, model)
         return Response("Image generated successfully", status=200)
